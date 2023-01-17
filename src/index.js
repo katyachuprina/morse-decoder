cde161878a90799e41cf9dcf89b76a21ed0ddb30
Katya Chuprina <katyachuprina@MacBook-Air-Katya.local>
2023-01-17 22:14:21 +0300

    docs: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let newArray = [];
+    let codeArray = [];
+    let codePhrase = '';
+
+    for (let i = 0; i < expr.length; i = i + 10) {
+      newArray.push(expr.slice(i, i + 10));
+    }
+
+    for (let i = 0; i < newArray.length; i++) {
+      let symbol = +(newArray[i]); //10101010
+      symbol = symbol + '';
+      let code = '';
+
+      for (let i = 0; i < symbol.length; i = i + 2) {
+        if ((symbol[i] + symbol[i + 1]) == 10) {
+          code += '.';
+        } else if ((symbol[i] + symbol[i + 1]) == 11) {
+          code += '-';
+        } else if ((symbol[i] + symbol[i + 1]) == '**') {
+          code = ' ';
+        }
+      }
+      codeArray.push(code);
+    }
+    
+    for (let i = 0; i < codeArray.length; i++) {
+      if (MORSE_TABLE[codeArray[i]] !== undefined) {
+        codePhrase += MORSE_TABLE[codeArray[i]];
+      } else {
+        codePhrase += ' ';
+      }
+    }
+
+    return codePhrase;
+} 
 
 module.exports = {
     decode
